e151a7563d4c8db6af6d154ae36cc3595e013013
martint <mart.tremblay.mt5@gmail.com>
2022-09-13 19:57:32 -0400

    Début de la création d'un thème personnalisé (exercice1).

---
+++ index.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * The main template file
+ *
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * E.g., it puts together the home page when no home.php file exists.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package underscrore
+ */
+?>
+
+<!doctype html>
+<html lang="fr">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport"
+	      content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>Document</title>
+</head>
+<body>
+    <main>
+        <?php
+            if ( have_posts() ) :
+                while(have_posts()):
+                    the_post();
+                    the_title();
+                    the_content();
+                endwhile;
+            endif;
+        ?>
+    </main>
+</body>
+</html>
+
