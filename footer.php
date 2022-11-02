9d9587945d0245258d6319c2c012319a9fe019d9
martint <mart.tremblay.mt5@gmail.com>
2022-11-02 12:19:19 -0400

    - suppression des 2 div contenant lorem3

--- footer.php
+++ footer.php
@@ -12,8 +12,6 @@
 	<footer class="site__footer">
 		<div><?php get_sidebar( 'footer-1' ); ?></div>
 		<div><?php get_sidebar( 'footer-2' ); ?></div>
-		<div>lorem3</div>
-		<div>lorem3</div>
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 <?php wp_footer(); ?>
